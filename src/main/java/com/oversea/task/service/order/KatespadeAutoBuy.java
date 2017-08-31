1d1a3aa9161718774c7ecb7af85a53e1edb58008
liuxf <liuxf@taofen8.com>
2017-08-31 15:05:52 +0800

    ks update

--- src/main/java/com/oversea/task/service/order/KatespadeAutoBuy.java
+++ src/main/java/com/oversea/task/service/order/KatespadeAutoBuy.java
@@ -1173,7 +1173,7 @@ public class KatespadeAutoBuy extends AutoBuy {
 					BigDecimal x = new BigDecimal(myPrice);
 					BigDecimal y = new BigDecimal(priceStr);
 					BigDecimal v = y.subtract(x);
-					if (v.doubleValue() > 5.00D){
+					if (v.doubleValue() > 6.00D){
 						logger.error("--->总价差距超过约定,不能下单");
 						return AutoBuyStatus.AUTO_PAY_TOTAL_GAP_OVER_APPOINT;
 					}
