033702fac6d020cde5dddf2c9ea77bc514872d79
liuxf <liuxf@taofen8.com>
2017-09-28 14:43:23 +0800

    维秘关闭弹窗

--- src/main/java/com/oversea/task/service/order/VictoriassecretAutoBuy.java
+++ src/main/java/com/oversea/task/service/order/VictoriassecretAutoBuy.java
@@ -80,15 +80,19 @@ public class VictoriassecretAutoBuy extends AutoBuy {
 		}
 		try {
 			TimeUnit.SECONDS.sleep(5);
+			logger.error("--->第一次关弹窗");
 			driver.executeScript("(function(){var els = document.getElementsByClassName('fsrDeclineButton');if(els && els[0]){els[0].click();}})();");
 			driver.executeScript("(function(){var els = document.getElementsByClassName('acsCloseButton');if(els && els[0]){els[0].click();}})();");
 			driver.executeScript("(function(){var els = document.getElementsByClassName('close');if(els && els[0]){els[0].click();}})();");
-		
-			TimeUnit.SECONDS.sleep(5);
 		} catch (Exception e) {
+		}
+		try {
+			TimeUnit.SECONDS.sleep(5);
+			logger.error("--->第二次关弹窗");
 			driver.executeScript("(function(){var els = document.getElementsByClassName('fsrDeclineButton');if(els && els[0]){els[0].click();}})();");
 			driver.executeScript("(function(){var els = document.getElementsByClassName('acsCloseButton');if(els && els[0]){els[0].click();}})();");
 			driver.executeScript("(function(){var els = document.getElementsByClassName('close');if(els && els[0]){els[0].click();}})();");
+		} catch (Exception e) {
 		}
 		
 		
