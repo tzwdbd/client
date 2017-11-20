12e379333b4c94f90882b8b70e03c9c7903b820c
liuxf <liuxf@taofen8.com>
2017-11-20 10:19:26 +0800

    bonpont 清空购物车修改

--- src/main/java/com/oversea/task/service/order/ManualBuy.java
+++ src/main/java/com/oversea/task/service/order/ManualBuy.java
@@ -235,7 +235,9 @@ public class ManualBuy{
 			
 		try {
 			logger.error(autoOrderCleanCart.getSiteName()+"--->确认购物车是否清空");
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(autoOrderCleanCart.getCleanEndCode())));
+			if(!StringUtil.isBlank(autoOrderCleanCart.getCleanEndCode())){
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(autoOrderCleanCart.getCleanEndCode())));
+			}
 		} catch (Exception e) {
 			logger.debug(autoOrderCleanCart.getSiteName()+"--->购物车不为空！");
 			return AutoBuyStatus.AUTO_CLEAN_CART_FAIL;
@@ -929,87 +931,96 @@ public class ManualBuy{
 		autoOrderLogin.setUsernameCode("#username");
 		autoOrderLogin.setSubmitCode("#loginBtn");
 		AutoBuyStatus status = auto.login("yunzh17@163.com", "tfb001001",autoOrderLogin);
+		AutoOrderCleanCart autoOrderCleanCart = new AutoOrderCleanCart();
+		autoOrderCleanCart.setCartLoadedCode(".cart-wrap");
+		autoOrderCleanCart.setCartUrl("https://www.bonpont.com/cart/index/index");
+		autoOrderCleanCart.setCleanEndCode("#emptyHandler");
+		autoOrderCleanCart.setCleanType(0);
+		autoOrderCleanCart.setConfirmCode("#easyDialogYesBtn");
+		autoOrderCleanCart.setRemoveCode(".delete");
+		autoOrderCleanCart.setSiteName("bonpont");
+		status = auto.cleanCart(autoOrderCleanCart);
 		System.out.println(status);
-		if (AutoBuyStatus.AUTO_LOGIN_SUCCESS.equals(status)){
-			RobotOrderDetail detail = new RobotOrderDetail();
-			detail.setProductRebateUrl("https://p.gouwuke.com/c?w=858413&c=19247&i=43784&pf=y&e=&t=https://www.bonpont.com/1003539.html");
-			detail.setUnits("CNY");
-			detail.setNum(2);
-			//detail.setPromotionCodeList("231231");
-			status = auto.selectProduct(detail, null);
-			RobotOrderDetail detail1 = new RobotOrderDetail();
-			detail1.setProductRebateUrl("https://p.gouwuke.com/c?w=858413&c=19247&i=43784&pf=y&e=&t=https://www.bonpont.com/1003346.html");
-			detail1.setUnits("CNY");
-			detail1.setNum(3);
-			//detail.setPromotionCodeList("231231");
-			status = auto.selectProduct(detail1, null);
-//			System.out.println(status);
-//			Map<String, String> param0 = new HashMap<String, String>();
-//			param0.put("my_price", "319.99");
-//			param0.put("count", "1");
-////			param0.put("isPay", String.valueOf(true));
-//			param0.put("cardNo", "4662 4833 6029 1396");
-//			UserTradeAddress userTradeAddress = new UserTradeAddress();
-//			userTradeAddress.setName("刘波");
-//			userTradeAddress.setAddress("西斗门路9号");
-//			userTradeAddress.setState("浙江省");
-//			userTradeAddress.setCity("杭州市");
-//			userTradeAddress.setDistrict("西湖区");
-//			userTradeAddress.setZip("310000");
-//			userTradeAddress.setIdCard("330881198506111918");
-//			userTradeAddress.setMobile("18668084980");
-//			OrderPayAccount orderPayAccount = new OrderPayAccount();
-//			orderPayAccount.setAccount("15268125960");
-//			orderPayAccount.setPayPassword("199027");
-//			status = auto.pay(detail,param0, userTradeAddress, orderPayAccount,"yunzh17@163.com", "tfb001001",autoOrderLogin);
-//			System.out.println(status);
-//			AutoOrderCleanCart autoOrderCleanCart = new AutoOrderCleanCart();
-//			autoOrderCleanCart.setCartLoadedCode(".delete-checked");
-//			autoOrderCleanCart.setCartUrl("http://cn.getthelabel.com/checkout/cart/");
-//			autoOrderCleanCart.setCleanEndCode(".cart-empty");
-//			autoOrderCleanCart.setCleanType(1);
-//			autoOrderCleanCart.setConfirmCode("#easyDialogYesBtn");
-//			autoOrderCleanCart.setRemoveCode(".delete-checked");
-//			autoOrderCleanCart.setSiteName("getthelabel");
-//			status = auto.cleanCart(autoOrderCleanCart);
-			//if(AutoBuyStatus.AUTO_CLEAN_CART_SUCCESS.equals(status)){
-				
-//				Map<String, String> param = new HashMap<String, String>();
-//		//		param.put("url", "http://cn.royyoungchemist.com.au/1131511.html/");
-//				param.put("url", "http://www.rebatesme.com/zh/click/?key=3b27c8fda4c3ff307a5ec0766a50d0a2&sitecode=haihu&showpage=0&partneruname=wenzhe@taofen8.com&checkcode=0995f957e0c90e5563b6b7cd85a70e03&targetUrl=http%3A%2F%2Fcn.getthelabel.com%2F1010315.html%2F");
-//				param.put("num", "1");
-//				param.put("sku", "[[\"color\",\"白色\"],[\"size\",\"XS\"]]");
-//				param.put("productEntityId", "4241869");
-//				auto.selectProduct(param);
-//				Map<String, String> param1 = new HashMap<String, String>();
-//		//		param.put("url", "http://cn.royyoungchemist.com.au/1131511.html/");
-//				param1.put("url", "http://www.rebatesme.com/zh/click/?key=3b27c8fda4c3ff307a5ec0766a50d0a2&sitecode=haihu&showpage=0&partneruname=wenzhe@taofen8.com&checkcode=0995f957e0c90e5563b6b7cd85a70e03&targetUrl=http%3A%2F%2Fcn.getthelabel.com%2F1023646.html");
-//				param1.put("num", "2");
-//				param1.put("sku", "[[\"color\",\"Blue\"],[\"size\",\"L\"]]");
-//				param1.put("productEntityId", "42418619");
-//				auto.selectProduct(param1);
-//				if(AutoBuyStatus.AUTO_SKU_SELECT_SUCCESS.equals(status)){
-//					Map<String, String> param0 = new HashMap<String, String>();
-//					param0.put("my_price", "39.99");
-//					param0.put("count", "1");
-////					param0.put("isPay", String.valueOf(true));
-//					param0.put("cardNo", "4662 4833 6029 1396");
-//					UserTradeAddress userTradeAddress = new UserTradeAddress();
-//					userTradeAddress.setName("刘波");
-//					userTradeAddress.setAddress("西斗门路9号");
-//					userTradeAddress.setState("浙江省");
-//					userTradeAddress.setCity("杭州市");
-//					userTradeAddress.setDistrict("西湖区");
-//					userTradeAddress.setZip("310000");
-//					userTradeAddress.setIdCard("330881198506111918");
-//					userTradeAddress.setMobile("18668084980");
-//					OrderPayAccount orderPayAccount = new OrderPayAccount();
-//					orderPayAccount.setAccount("15268125960");
-//					orderPayAccount.setPayPassword("199027");
-//					status = auto.pay(param0,userTradeAddress,orderPayAccount);
-//				}
-			//}
-		}
+//		if (AutoBuyStatus.AUTO_LOGIN_SUCCESS.equals(status)){
+//			RobotOrderDetail detail = new RobotOrderDetail();
+//			detail.setProductRebateUrl("https://p.gouwuke.com/c?w=858413&c=19247&i=43784&pf=y&e=&t=https://www.bonpont.com/1003539.html");
+//			detail.setUnits("CNY");
+//			detail.setNum(2);
+//			//detail.setPromotionCodeList("231231");
+//			status = auto.selectProduct(detail, null);
+//			RobotOrderDetail detail1 = new RobotOrderDetail();
+//			detail1.setProductRebateUrl("https://p.gouwuke.com/c?w=858413&c=19247&i=43784&pf=y&e=&t=https://www.bonpont.com/1003346.html");
+//			detail1.setUnits("CNY");
+//			detail1.setNum(3);
+//			//detail.setPromotionCodeList("231231");
+//			status = auto.selectProduct(detail1, null);
+////			System.out.println(status);
+////			Map<String, String> param0 = new HashMap<String, String>();
+////			param0.put("my_price", "319.99");
+////			param0.put("count", "1");
+//////			param0.put("isPay", String.valueOf(true));
+////			param0.put("cardNo", "4662 4833 6029 1396");
+////			UserTradeAddress userTradeAddress = new UserTradeAddress();
+////			userTradeAddress.setName("刘波");
+////			userTradeAddress.setAddress("西斗门路9号");
+////			userTradeAddress.setState("浙江省");
+////			userTradeAddress.setCity("杭州市");
+////			userTradeAddress.setDistrict("西湖区");
+////			userTradeAddress.setZip("310000");
+////			userTradeAddress.setIdCard("330881198506111918");
+////			userTradeAddress.setMobile("18668084980");
+////			OrderPayAccount orderPayAccount = new OrderPayAccount();
+////			orderPayAccount.setAccount("15268125960");
+////			orderPayAccount.setPayPassword("199027");
+////			status = auto.pay(detail,param0, userTradeAddress, orderPayAccount,"yunzh17@163.com", "tfb001001",autoOrderLogin);
+////			System.out.println(status);
+////			AutoOrderCleanCart autoOrderCleanCart = new AutoOrderCleanCart();
+////			autoOrderCleanCart.setCartLoadedCode(".delete-checked");
+////			autoOrderCleanCart.setCartUrl("http://cn.getthelabel.com/checkout/cart/");
+////			autoOrderCleanCart.setCleanEndCode(".cart-empty");
+////			autoOrderCleanCart.setCleanType(1);
+////			autoOrderCleanCart.setConfirmCode("#easyDialogYesBtn");
+////			autoOrderCleanCart.setRemoveCode(".delete-checked");
+////			autoOrderCleanCart.setSiteName("getthelabel");
+////			status = auto.cleanCart(autoOrderCleanCart);
+//			//if(AutoBuyStatus.AUTO_CLEAN_CART_SUCCESS.equals(status)){
+//				
+////				Map<String, String> param = new HashMap<String, String>();
+////		//		param.put("url", "http://cn.royyoungchemist.com.au/1131511.html/");
+////				param.put("url", "http://www.rebatesme.com/zh/click/?key=3b27c8fda4c3ff307a5ec0766a50d0a2&sitecode=haihu&showpage=0&partneruname=wenzhe@taofen8.com&checkcode=0995f957e0c90e5563b6b7cd85a70e03&targetUrl=http%3A%2F%2Fcn.getthelabel.com%2F1010315.html%2F");
+////				param.put("num", "1");
+////				param.put("sku", "[[\"color\",\"白色\"],[\"size\",\"XS\"]]");
+////				param.put("productEntityId", "4241869");
+////				auto.selectProduct(param);
+////				Map<String, String> param1 = new HashMap<String, String>();
+////		//		param.put("url", "http://cn.royyoungchemist.com.au/1131511.html/");
+////				param1.put("url", "http://www.rebatesme.com/zh/click/?key=3b27c8fda4c3ff307a5ec0766a50d0a2&sitecode=haihu&showpage=0&partneruname=wenzhe@taofen8.com&checkcode=0995f957e0c90e5563b6b7cd85a70e03&targetUrl=http%3A%2F%2Fcn.getthelabel.com%2F1023646.html");
+////				param1.put("num", "2");
+////				param1.put("sku", "[[\"color\",\"Blue\"],[\"size\",\"L\"]]");
+////				param1.put("productEntityId", "42418619");
+////				auto.selectProduct(param1);
+////				if(AutoBuyStatus.AUTO_SKU_SELECT_SUCCESS.equals(status)){
+////					Map<String, String> param0 = new HashMap<String, String>();
+////					param0.put("my_price", "39.99");
+////					param0.put("count", "1");
+//////					param0.put("isPay", String.valueOf(true));
+////					param0.put("cardNo", "4662 4833 6029 1396");
+////					UserTradeAddress userTradeAddress = new UserTradeAddress();
+////					userTradeAddress.setName("刘波");
+////					userTradeAddress.setAddress("西斗门路9号");
+////					userTradeAddress.setState("浙江省");
+////					userTradeAddress.setCity("杭州市");
+////					userTradeAddress.setDistrict("西湖区");
+////					userTradeAddress.setZip("310000");
+////					userTradeAddress.setIdCard("330881198506111918");
+////					userTradeAddress.setMobile("18668084980");
+////					OrderPayAccount orderPayAccount = new OrderPayAccount();
+////					orderPayAccount.setAccount("15268125960");
+////					orderPayAccount.setPayPassword("199027");
+////					status = auto.pay(param0,userTradeAddress,orderPayAccount);
+////				}
+//			//}
+//		}
 	}
 	
 	protected static Set<String> getPromotionList(String promotionStr){
